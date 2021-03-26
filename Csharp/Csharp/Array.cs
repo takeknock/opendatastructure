fa58c6251d13e11e02f2d52d350e277c447c9d69
mmatthew <takeknock@gmail.com>
2021-03-27 00:00:05 +0900

    feat: add Array class

---
+++ Csharp/Csharp/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp
+{
+    public class Array<T> : ICloneable
+    {
+        private T[] _a;
+        public int Length { get; private set; }
+        public Array(int len)
+        {
+            Length = len;
+            _a = new T[len];
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i < 0 || i >= Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return _a[i];
+            }
+        }
+
+        object ICloneable.Clone()
+        {
+            Array<T> next = new Array<T>(Length);
+            next._a = this._a.Clone() as T[];
+            return next;
+        }
+    }
+}
---
+++ Csharp/Csharp/CloneableExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp
+{
+    static class CloneableExtension
+    {
+        public static T CloneEx<T>(this T src) where T : class, ICloneable
+          => src.Clone() as T;
+    }
+}
