9eeb90f65316327895dff800e2a2a849df42d86f
mmatthew <takeknock@gmail.com>
2021-04-07 00:05:19 +0900

    feat: add GaussJordan

---
+++ Csharp/Csharp/Algorithm/GaussJordan.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Algorithm
+{
+    public class GaussJordan
+    {
+        /// <summary>
+        /// Solve Ax=b
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static public Array<double> Solve(Array<Array<double>> A, Array<double> b)
+        {
+            int n = A.Length;
+            Array<Array<double>> B = new Array<Array<double>>(n);
+            for (int i = 0; i < n; i++)
+            {
+                B[i] = new Array<double>(n + 1);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    B[i][j] = A[i][j];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                B[i][n] = b[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (Math.Abs(B[i][i]) < 1E-8)
+                {
+                    return new Array<double>(n);
+                }
+                for (int j = i+1; j <= n; j++)
+                {
+                    B[i][j] /= B[i][i];
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        for (int k = i+1 ; k <= n; k++)
+                        {
+                            B[j][k] -= B[i][j] * B[i][k];
+                        }
+                    }
+                }
+            }
+            Array<double> x = new Array<double>(n);
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = B[i][n];
+            }
+            return x;
+        }
+    }
+}
---
+++ Csharp/Test/GaussJordanTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class GaussJordanTest
+    {
+        [TestMethod]
+        public void Solve()
+        {
+            Csharp.Array<Csharp.Array<double>> A = new Csharp.Array<Csharp.Array<double>>(3);
+            A[0] = new Csharp.Array<double>(3);
+            A[1] = new Csharp.Array<double>(3);
+            A[2] = new Csharp.Array<double>(3);
+            A[0][0] = 1;
+            A[0][1] = -2;
+            A[0][2] = 3;
+            A[1][0] = 4;
+            A[1][1] = -5;
+            A[1][2] = 6;
+            A[2][0] = 7;
+            A[2][1] = -8;
+            A[2][2] = 10;
+            Csharp.Array<double> b = new Csharp.Array<double>(3);
+            b[0] = 6;
+            b[1] = 12;
+            b[2] = 21;
+            var answer = Csharp.Algorithm.GaussJordan.Solve(A, b);
+            System.Console.WriteLine(answer[0]);
+            System.Console.WriteLine(answer[1]);
+            System.Console.WriteLine(answer[2]);
+        }
+    }
+}
